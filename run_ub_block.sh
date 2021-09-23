853e60055c6c1b45d6681a03eafecbd1e5167746
Nicolo Vallarano <nicolo.vallarano@business.uzh.ch>
2021-09-23 09:03:56 +0000

    updated readme

--- run_ub_block.sh
+++ run_ub_block.sh
@@ -5,4 +5,4 @@
 # python3 ub_diffusion_net.py --curr=BTC --heur=1 --freq=day
 echo "heur 2"
 #python3 ub_ground_truth.py --curr=BTC --heur=2 --output=uniform_black
-python3 ub_diffusion_block.py --curr=BTC --heur=2 --freq=day
+python3 ub_diffusion_block.py --curr=BTC --heur=2 --output=uniform_black
